15e3989853342e6305d3751c43a57bfab3b405d6
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-02 20:12:15 -0300

    Create Retangulo.java

---
+++ Questao2/Retangulo.java
@@ -0,0 +1,27 @@
+//visibilidade | palavra chave | nome
+
+public class Retangulo{
+    
+    //atributo
+    
+    //tipo | nome | valor
+    int largura = 2;
+    int comprimento= 4;
+    
+    // metodo
+    
+    //visibilidade | retorno | nome
+    
+    public int calcularArea(int C, int L){
+        return C * L;
+    }
+    
+    public int calcularPerimetro(int L, int C){
+        return (2 * C) + (2 * L);
+    }
+    
+    public void imprimir(int A, int P){
+        System.out.print(A + " " + P);
+    }
+    
+}
