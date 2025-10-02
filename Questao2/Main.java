ccd65c0aabd801c2f297899bcfb35c0cd2738503
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-02 20:10:49 -0300

    Update Main.java

--- Questao2/Main.java
+++ Questao2/Main.java
@@ -1,1 +1,20 @@
-
+public class Main{
+    
+    public static void main (String[] args) {
+        
+        //objeto
+        //tipo| nome | new | classe
+        Retangulo r = new Retangulo();
+        
+        int l = r.largura;
+        int c = r.comprimento;
+        
+        int area = r.calcularArea(l, c);
+        
+        int perimetro = r.calcularPerimetro(l, c);
+        
+        r.imprimir(area, perimetro);
+        
+    }
+    
+}
