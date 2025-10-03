1eb26ed2a7750cfa71a627e3adb3ef2379993a11
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-03 14:37:19 -0300

    Update Main.java

--- Questao3/Main.java
+++ Questao3/Main.java
@@ -1,1 +1,11 @@
-main
+public class Main{
+    
+    public static void main (String[] args) {
+        
+        Circulo retangulo = new Circulo(360);
+
+        retangulo.imprimir();
+        
+    }
+    
+}
